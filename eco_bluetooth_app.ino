8da438e7bef4ad32907c9667cc660405446a7800
LunaLee125 <71402137+LunaLee125@users.noreply.github.com>
2023-10-29 11:15:19 -0400

    Add files via upload

---
+++ eco_bluetooth_app.ino
@@ -0,0 +1,168 @@
+#include <SoftwareSerial.h> // library for bluetooth
+
+#include <Wire.h> // library for motorshield
+#include <Adafruit_MotorShield.h> // library for motorshield
+#include "utility/Adafruit_MS_PWMServoDriver.h" // library for motorshield
+
+// create new arduino shield object 
+Adafruit_MotorShield arduinoShield = Adafruit_MotorShield(); 
+
+// motor setup
+Adafruit_DCMotor *leftThruster = arduinoShield.getMotor(4);
+Adafruit_DCMotor *rightThruster = arduinoShield.getMotor(1);
+Adafruit_DCMotor *conveyorBelt = arduinoShield.getMotor(3);
+Adafruit_DCMotor *conveyorBeltTilt = arduinoShield.getMotor(2);
+int last = 0;
+
+// bluetooth setup
+SoftwareSerial MyBlue(2,5); // set RX and TX
+String msg = "o"; //used for bluetooth commands
+
+// ping sensor setup
+int distance2pin = 11;
+int distance3pin = 10;
+
+void setup() {
+  
+  Serial.begin(9600); // set up serial monitor
+  MyBlue.begin(9600); // set up bluetooth serial monitor
+
+  arduinoShield.begin(); // set up motor shield
+
+}
+
+void loop()
+{
+  
+  
+  checkDistance();
+  motorControl();
+  
+}
+
+// method that controls motors
+void motorControl(){
+
+  // if connected with bluetooth
+  if(MyBlue.available() > 0);
+
+    // get input from bluetooth
+    msg = MyBlue.readString();
+    
+    // truncate to only have useful information
+    msg = msg.substring(0, msg.length()-1);
+    msg = msg.substring(msg.lastIndexOf(" ") + 1);
+
+    // find which motor is being moved (LT for left thruster, RT fro right thruster
+    // CT for conveyor belt tilt, CB for conveyor belt)
+    String type = msg.substring(0,2);
+    Serial.println(type);
+    // map the 0 to 100 seekBar value to the 0 to 255 motor values
+    int val = 255 * (msg.substring(2)).toInt() / 100;
+
+  
+  if(type.equals("LT")){
+    Serial.println("move left thruster");
+    leftThruster->setSpeed(val);
+    leftThruster->run(FORWARD);
+    
+  }
+  else if (type.equals("RT")){
+    Serial.println("move right thruster");
+    rightThruster->setSpeed(val);
+    rightThruster->run(FORWARD);
+    
+  }
+  else if(type.equals("CT")){
+    
+    Serial.println("move conveyor belt tilt");
+    
+    int x = map(val, 0,255, 0,3);
+    
+    Serial.print(x);
+    Serial.print(" ");
+    Serial.print(last);
+    Serial.println();
+    
+    if(x > last){
+      for(int i = last; i < x; i++){
+        conveyorBeltTilt->setSpeed(500);
+       conveyorBeltTilt->run(BACKWARD);
+       delay(1000);
+      }
+      conveyorBeltTilt->setSpeed(0);
+       conveyorBeltTilt->run(FORWARD);
+      
+    } else if (x < last){
+      for(int i = last; i >= x; i--){
+        conveyorBeltTilt->setSpeed(500);
+       conveyorBeltTilt->run(FORWARD);
+       delay(1000);
+      }
+      conveyorBeltTilt->setSpeed(0);
+       conveyorBeltTilt->run(FORWARD);
+    }
+    last = x;
+  
+  }
+  else if (type.equals("CB")){
+    Serial.println("move conveyor belt");
+    Serial.println("ok");
+    conveyorBelt->setSpeed(val);
+    conveyorBelt->run(FORWARD);
+    
+  } 
+  
+}
+
+// method to 
+void checkDistance(){
+  
+  int distanceThreshold = 20;
+
+  // measure the ping time in centimeters and convert to inches
+  int inchesRight = (0.01723 * readUltrasonicDistance(distance2pin) / 2.54);
+  int inchesLeft = (0.01723 * readUltrasonicDistance(distance3pin) / 2.54);
+
+  Serial.println(inchesRight);
+  Serial.println(inchesLeft);
+
+  warning(inchesLeft, "l");
+  delay(10);
+  warning(inchesRight, "r");
+
+}
+
+// method to get input from ultrasonic sensor
+long readUltrasonicDistance(int sig) {
+  
+  pinMode(sig, OUTPUT);  // Clear the trigger
+  digitalWrite(sig, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(sig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sig, LOW);
+  pinMode(sig, INPUT);
+  
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(sig, HIGH);
+}
+
+void warning(int inches, String d){
+
+  Serial.println(inches);
+
+  int distanceThreshold = 20;
+
+  if (inches > distanceThreshold) {
+    MyBlue.println(d + "is safe!");
+  }
+  if (inches <= distanceThreshold && inches > distanceThreshold - 15) {
+    MyBlue.println(d + "is close to an object!");
+  }
+  if (inches < distanceThreshold - 15) {
+    MyBlue.println(d + "is in danger!");
+  }
+}
